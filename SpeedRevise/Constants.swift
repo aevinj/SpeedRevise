df74d2f68fc412bb5655549e42ac39084de4c3df
aevinj <ajjaevinjais@gmail.com>
2024-06-10 17:24:37 +0100

    Connected openAPI

---
+++ SpeedRevise/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Consants.swift
+//  SpeedRevise
+//
+//  Created by Aevin Jais on 10/06/2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let openAIAPIKey = "sk-proj-NXDU5wIsCbdI0eF9redzT3BlbkFJQVpLakYKnrlq2V0a7ytc"
+}
--- SpeedRevise/Views/AddTopicView.swift
+++ SpeedRevise/Views/AddTopicView.swift
@@ -9,43 +9,83 @@ import SwiftUI
 
 struct AddTopicView: View {
     @Environment(\.dismiss) var dismiss
-    @State var prompt: String = ""
+    @StateObject private var chatViewModel = ChatViewModel()
     
-    var body: some View {
-        ZStack {
-            Color("BackgroundColor")
-                .ignoresSafeArea()
-            
-            VStack {
-                HStack {
-                    Text("New topic")
-                        .font(.system(size: 32, weight: .medium))
-                    
-                    Spacer()
-                    
-                    Button {
-                        dismiss()
-                    } label: {
-                        Image(systemName: "chevron.left")
-                            .foregroundStyle(Color("BGCFlipped"))
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundStyle(Color.primary)
-                            .shadow(radius: 50)
-                            .frame(width: 55, height: 55)
-                            .background(.ultraThinMaterial)
-                            .clipShape(RoundedRectangle(cornerRadius: 20))
-                            .padding(.trailing, 16)
+        var body: some View {
+            ZStack {
+                Color("BackgroundColor")
+                    .ignoresSafeArea()
+    
+                VStack {
+                    HStack {
+                        Text("New topic")
+                            .font(.system(size: 32, weight: .medium))
+    
+                        Spacer()
+    
+                        Button {
+                            dismiss()
+                        } label: {
+                            Image(systemName: "chevron.left")
+                                .foregroundStyle(Color("BGCFlipped"))
+                                .font(.system(size: 20, weight: .medium))
+                                .foregroundStyle(Color.primary)
+                                .shadow(radius: 50)
+                                .frame(width: 55, height: 55)
+                                .background(.ultraThinMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .padding(.trailing, 16)
+                        }
                     }
-                }
-                .padding(EdgeInsets(top: 32, leading: 32, bottom: 0, trailing: 0))
-                
-                Spacer()
-                
-                TextInputView(textInput: $prompt, prompt: "Enter a topic to revise...")
+                    .padding(EdgeInsets(top: 32, leading: 32, bottom: 0, trailing: 0))
+    
+                    Spacer()
+    
+                    ScrollView {
+                        ForEach(chatViewModel.messages.filter({$0.role != .system}), id: \.id) { message in
+                            MessageView(message: message)
+                        }
+                    }
+    
+                    HStack {
+                        TextInputView(textInput: $chatViewModel.currentInput, prompt: "Enter a topic to revise...")
+                            .padding(.trailing, 2)
+    
+                        Button {
+                            chatViewModel.sendMessage()
+                        } label: {
+                            Image(systemName: "chevron.right")
+                                .foregroundStyle(Color("BGCFlipped"))
+                                .font(.system(size: 20, weight: .medium))
+                                .foregroundStyle(Color.primary)
+                                .shadow(radius: 50)
+                                .frame(width: 55, height: 55)
+                                .background(.ultraThinMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .padding(.trailing, 16)
+                        }
+                    }
                     .padding(.bottom, 100)
+                }
+            }
+            .navigationBarBackButtonHidden(true)
+        }
+}
+
+struct MessageView: View {
+    var message: Message
+    var body: some View {
+        HStack {
+            if message.role == .user {
+                Spacer()
+            }
+            
+            Text(message.content)
+            
+            if message.role == .assistant {
+                Spacer()
             }
         }
-        .navigationBarBackButtonHidden(true)
     }
 }
 
