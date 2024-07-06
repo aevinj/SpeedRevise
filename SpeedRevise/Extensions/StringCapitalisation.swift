40c1052cfa495adef01b607920704ad30bcc2348
aevinj <ajjaevinjais@gmail.com>
2024-07-06 21:15:48 +0100

    fix capitalisation error

--- SpeedRevise/Extensions/StringCapitalisation.swift
+++ SpeedRevise/Extensions/StringCapitalisation.swift
@@ -8,6 +8,6 @@
 extension String {
     var capitalizedFirst: String {
         guard let first = self.first else { return self }
-        return first.uppercased() + self.dropFirst().lowercased()
+        return first.uppercased() + self.dropFirst()
     }
 }
---
+++ SpeedRevise/Views/Subjects/Topics/Notes/ViewNoteViewSettingsMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  ViewNoteViewSettingsMenuView.swift
+//  SpeedRevise
+//
+//  Created by Aevin Jais on 06/07/2024.
+//
+
+import SwiftUI
+
+struct ViewNoteViewSettingsMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewNoteViewSettingsMenuView()
+}
