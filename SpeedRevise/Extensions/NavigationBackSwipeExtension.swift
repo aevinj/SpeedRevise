4f3186707260885628e6a209cafa4ee478c19753
aevinj <ajjaevinjais@gmail.com>
2024-07-09 19:33:36 +0100

    add swipe to go back functionality

---
+++ SpeedRevise/Extensions/NavigationBackSwipeExtension.swift
@@ -0,0 +1,19 @@
+//
+//  NavigationBackSwipeExtension.swift
+//  SpeedRevise
+//
+//  Created by Aevin Jais on 09/07/2024.
+//
+
+import SwiftUI
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
